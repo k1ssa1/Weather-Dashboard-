5ec59a4269e06218bdbb146895e4af56fba45289
k1ssa1 <samikissai.edu@gmail.com>
2024-08-23 22:55:22 +0200

    chore: uninstall vuetify

--- src/Index.vue
+++ src/Index.vue
@@ -1,9 +1,22 @@
 <script setup>
-import Navbar from "./components/Navbar.vue"
 </script>
 
 <template>
-  <Navbar/>
+   <v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-btn color="primary">Button</v-btn>
+          <v-card>
+            <v-card-title>Hello World!</v-card-title>
+            <v-card-text>
+              This is a Vuetify component.
+            </v-card-text>
+          </v-card>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <style scoped>
